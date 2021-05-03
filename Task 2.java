fb776eac43bbff44d00a7b9d33879e80de976fca
ElliNotiQ <83469385+ElliNotiQ@users.noreply.github.com>
2021-05-03 16:37:57 +0300

    Create Task 2.java

---
+++ Task 2.java
@@ -0,0 +1,19 @@
+package ALevel;
+import java.util.Scanner;
+
+public class Main {
+
+
+	public static void main(String[]Object) {
+
+        Scanner number = new Scanner(System.in);
+        int a = number.nextInt();
+    }
+
+        public static void number.nextInt(int) {
+            if (a % 2 == 0)
+                System.out.println("четное");
+            else
+                System.out.println("не четное");
+    }
+}
